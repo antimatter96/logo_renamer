d5640b350464f6b456426ae80a9963b9dc2d3e21
Arpit Jain <me.arpitjain09@gmail.com>
2026-02-11 13:54:30 +0530

    rename script

--- rename.sh
+++ rename.sh
@@ -1,6 +1,8 @@
 rename 's/_(result|trimmed|extended|part_)//ig' -- *
 rename 's/_\././ig' -- *
 rename 's/^Screenshot_//ig' -- *
+rename 's/^SCR-//ig' -- *
 rename 's/__+/_/ig' -- *
 rename 's/_Copy_/_/ig' -- *
 rename 's/[\(\) ]/_/ig' -- *
+rename 's/^_+//ig' -- *
